271d807f745ecb6b553e8e31d7fcdaf9cd0f22ae
brGuirra <br.guirra@gmail.com>
2022-01-17 19:38:08 -0300

    Initial Commit

---
+++ challenge-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-2
+//
+//  Created by Bruno Guirra on 17/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
