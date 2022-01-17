d110b3676f2d14f98d61827753a5ab0cd2b97494
brGuirra <br.guirra@gmail.com>
2022-01-17 20:14:44 -0300

    feat: add the uiTableViewController and the navigationController

--- challenge-2/ViewController.swift
+++ challenge-2/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var shoppingListItens = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shopping List"
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingListItens.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingItem", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = shoppingListItens[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
 }
 
