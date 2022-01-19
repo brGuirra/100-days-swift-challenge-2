c395831fc681e44ca7f9b2ed446594890f999854
brGuirra <br.guirra@gmail.com>
2022-01-18 21:04:53 -0300

    feat: add ui elements to interface
    add a UIButton responsible to add items to the shopping list
    add a leftBarButtonItem to clear the items in the list
    add a rightBarButtonItem to share the list

--- challenge-2/ViewController.swift
+++ challenge-2/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         title = "Shopping List"
+        let titleAttributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .bold)
+        ]        
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(clearList))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareList))
+        
+        let addButton = createButton()
+        
+        view.addSubview(addButton)
+        
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            addButton.widthAnchor.constraint(equalToConstant: 80),
+            addButton.heightAnchor.constraint(equalToConstant: 80),
+            addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,6 +48,31 @@ class ViewController: UITableViewController {
         content.text = shoppingListItens[indexPath.row]
         cell.contentConfiguration = content
         return cell
+    }
+    
+    
+    @objc func clearList() {
+        
+    }
+    
+    @objc func shareList() {
+
+    }
+    
+    func addItem() -> UIAction {
+        return UIAction() { _ in
+        }
+    }
+    
+    func createButton() -> UIButton {
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 160, y: 160, width: 80, height: 80)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.setImage(UIImage(systemName: "plus.circle.fill") , for: .normal)
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        button.addAction(addItem(), for: .touchUpInside)
+        return button
     }
 }
 
